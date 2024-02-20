0422197a39c6fda8fb493595e44e6f7a0cff2e6b
Nael-Sayegh <naelousay@gmail.com>
2024-02-20 08:38:47 +0100

    Pass the shortcut if no version found

--- addon/appModules/filezilla.py
+++ addon/appModules/filezilla.py
@@ -13,17 +13,21 @@ import globalVars
 
 addonHandler.initTranslation()
 class AppModule(appModuleHandler.AppModule):
-	@scriptHandler.script(gesture="kb:control+f6",description=_("FileZilla: Switch between local and remote file lists; when not in a list, navigate to the remote file list."),category="FileZilla")
-	def script_switchList(self, gesture):
-		fo = api.getNavigatorObject()
-		if fo.role == controlTypes.Role.LISTITEM:
-			fo=fo.parent
-		if fo.role == controlTypes.Role.LIST and fo.windowClassName == 'SysListView32' and fo.windowControlID == -31811:
-			goRemoteList()
-		elif fo.role == controlTypes.Role.LIST and fo.windowClassName == 'SysListView32' and fo.windowControlID == -31806:
-			goLocalList()
-		else:
-			goRemoteList()
+	try:
+		globalVars.foregroundObject.appModule.productVersion
+		@scriptHandler.script(gesture="kb:control+f6",description=_("FileZilla: Switch between local and remote file lists; when not in a list, navigate to the remote file list."),category="FileZilla")
+		def script_switchList(self, gesture):
+			fo = api.getNavigatorObject()
+			if fo.role == controlTypes.Role.LISTITEM:
+				fo=fo.parent
+			if fo.role == controlTypes.Role.LIST and fo.windowClassName == 'SysListView32' and fo.windowControlID == -31811:
+				goRemoteList()
+			elif fo.role == controlTypes.Role.LIST and fo.windowClassName == 'SysListView32' and fo.windowControlID == -31806:
+				goLocalList()
+			else:
+				goRemoteList()
+	except RuntimeError:
+		pass
 	
 	@scriptHandler.script(gesture="kb:control+shift+h",description=_("FileZilla: go to the connections history button"),category="FileZilla")
 	def script_clickHistory(self, gesture):
@@ -62,7 +66,9 @@ class StatusText(IAccessible):
 	
 	@scriptHandler.script(gesture="kb:shift+tab")
 	def script_previousText(self, gesture):
+		#Je récupère l'objet en avant plan
 		fg = api.getForegroundObject()
+		#Je parcours l'arborescence des objets pour arriver à la liste.
 		o = getChildByID(fg, ID = -31832, nb=1)
 		o = getChildByID(o, ID = -31832, nb=2)
 		o = getChildByID(o, ID = -31943, nb=3)
@@ -82,85 +88,83 @@ def getChildByID(o, ID, nb):
 
 def goLocalList():
 	fg=api.getForegroundObject()
-	try:
-		if int(globalVars.foregroundObject.appModule.productVersion.split(",")[1]) < 65:
-			o = getChildByID(fg, ID = -31828, nb=1)
-			o = getChildByID(o, ID = -31828, nb=2)
-			o = getChildByID(o, ID = -31827, nb=3)
-			o = getChildByID(o, ID = -31827, nb=4)
-			o = getChildByID(o, ID = -31817, nb=5)
-			o = getChildByID(o, ID = -31817, nb=6)
-			o = getChildByID(o, ID = -31816, nb=7)
-			o = getChildByID(o, ID = -31816, nb=8)
-			o = getChildByID(o, ID = -31815, nb=9)
-			o = getChildByID(o, ID = -31815, nb=10)
-			o = getChildByID(o, ID = -31812, nb=11)
-			o = getChildByID(o, ID = -31812, nb=12)
-			o = getChildByID(o, ID = -31810, nb=13)
-			o = getChildByID(o, ID = -31810, nb=14)
-			o.setFocus()
-		else:
-			fg = api.getForegroundObject()
-			o = getChildByID(fg, ID=-31828, nb=1)
-			o = getChildByID(o, ID=-31828, nb=2)
-			o=getChildByID(o, ID=-31827, nb=3)
-			o=getChildByID(o, ID=-31827, nb=4)
-			o=getChildByID(o, ID=-31818, nb=5)
-			o=getChildByID(o, ID=-31818, nb=6)
-			o=getChildByID(o, ID=-31817, nb=7)
-			o=getChildByID(o, ID=-31817, nb=8)
-			o=getChildByID(o, ID=-31816, nb=9)
-			o=getChildByID(o, ID=-31816, nb=10)
-			o=getChildByID(o, ID=-31813, nb=11)
-			o=getChildByID(o, ID=-31813, nb=12)
-			o=getChildByID(o, ID=-31811, nb=13)
-			o=getChildByID(o, ID=-31811, nb=14) 
-			o.setFocus()
-	except No version information:
-		pass
+	if int(globalVars.foregroundObject.appModule.productVersion.split(",")[1]) < 65:
+		o = getChildByID(fg, ID = -31828, nb=1)
+		o = getChildByID(o, ID = -31828, nb=2)
+		o = getChildByID(o, ID = -31827, nb=3)
+		o = getChildByID(o, ID = -31827, nb=4)
+		o = getChildByID(o, ID = -31817, nb=5)
+		o = getChildByID(o, ID = -31817, nb=6)
+		o = getChildByID(o, ID = -31816, nb=7)
+		o = getChildByID(o, ID = -31816, nb=8)
+		o = getChildByID(o, ID = -31815, nb=9)
+		o = getChildByID(o, ID = -31815, nb=10)
+		o = getChildByID(o, ID = -31812, nb=11)
+		o = getChildByID(o, ID = -31812, nb=12)
+		o = getChildByID(o, ID = -31810, nb=13)
+		o = getChildByID(o, ID = -31810, nb=14)
+		#Je vérifie si l'objet dans la liste qui indique que l'on est pas connecté est là
+		o.setFocus()
+	else:
+		fg = api.getForegroundObject()
+		o = getChildByID(fg, ID=-31828, nb=1)
+		o = getChildByID(o, ID=-31828, nb=2)
+		o=getChildByID(o, ID=-31827, nb=3)
+		o=getChildByID(o, ID=-31827, nb=4)
+		o=getChildByID(o, ID=-31818, nb=5)
+		o=getChildByID(o, ID=-31818, nb=6)
+		o=getChildByID(o, ID=-31817, nb=7)
+		o=getChildByID(o, ID=-31817, nb=8)
+		o=getChildByID(o, ID=-31816, nb=9)
+		o=getChildByID(o, ID=-31816, nb=10)
+		o=getChildByID(o, ID=-31813, nb=11)
+		o=getChildByID(o, ID=-31813, nb=12)
+		o=getChildByID(o, ID=-31811, nb=13)
+		o=getChildByID(o, ID=-31811, nb=14) 
+		o.setFocus()
 
 def goRemoteList():
 	fg=api.getForegroundObject()
-	try:
-		if int(globalVars.foregroundObject.appModule.productVersion.split(",")[1]) < 65:
-			o = getChildByID(fg, ID = -31828, nb=1)
-			o = getChildByID(o, ID = -31828, nb=2)
-			o = getChildByID(o, ID = -31827, nb=3)
-			o = getChildByID(o, ID = -31827, nb=4)
-			o = getChildByID(o, ID = -31817, nb=5)
-			o = getChildByID(o, ID = -31817, nb=6)
-			o = getChildByID(o, ID = -31816, nb=7)
-			o = getChildByID(o, ID = -31816, nb=8)
-			o = getChildByID(o, ID = -31814, nb=9)
-			o = getChildByID(o, ID = -31814, nb=10)
-			o = getChildByID(o, ID = -31807, nb=11)
-			o = getChildByID(o, ID = -31807, nb=12)
-			o = getChildByID(o, ID = -31805, nb=13)
-			o = getChildByID(o, ID = -31805, nb=14)
-			contentList = getChildByID(o, ID = -31804, nb=15)
-			if contentList:
-				ui.message(_("The list of remote files cannot be found"))
-			else:
-				o.setFocus()
+	if int(globalVars.foregroundObject.appModule.productVersion.split(",")[1]) < 65:
+		o = getChildByID(fg, ID = -31828, nb=1)
+		o = getChildByID(o, ID = -31828, nb=2)
+		o = getChildByID(o, ID = -31827, nb=3)
+		o = getChildByID(o, ID = -31827, nb=4)
+		o = getChildByID(o, ID = -31817, nb=5)
+		o = getChildByID(o, ID = -31817, nb=6)
+		o = getChildByID(o, ID = -31816, nb=7)
+		o = getChildByID(o, ID = -31816, nb=8)
+		o = getChildByID(o, ID = -31814, nb=9)
+		o = getChildByID(o, ID = -31814, nb=10)
+		o = getChildByID(o, ID = -31807, nb=11)
+		o = getChildByID(o, ID = -31807, nb=12)
+		o = getChildByID(o, ID = -31805, nb=13)
+		o = getChildByID(o, ID = -31805, nb=14)
+		#Je vérifie si l'objet dans la liste qui indique que l'on est pas connecté est là
+		contentList = getChildByID(o, ID = -31804, nb=15)
+		if contentList:
+			ui.message(_("The list of remote files cannot be found"))
+		#Sinon on y place le focus
 		else:
-			o = getChildByID(fg, ID = -31828, nb=1)
-			o = getChildByID(o, ID = -31828, nb=2)
-			o = getChildByID(o, ID = -31827, nb=3)
-			o = getChildByID(o, ID = -31827, nb=4)
-			o = getChildByID(o, ID = -31818, nb=5)
-			o = getChildByID(o, ID = -31818, nb=6)
-			o = getChildByID(o, ID = -31817, nb=7)
-			o = getChildByID(o, ID = -31817, nb=8)
-			o = getChildByID(o, ID = -31815, nb=9)
-			o = getChildByID(o, ID = -31815, nb=10)
-			o = getChildByID(o, ID = -31808, nb=11)
-			o = getChildByID(o, ID = -31808, nb=12)
-			o = getChildByID(o, ID = -31806, nb=13)
-			o = getChildByID(o, ID = -31806, nb=14)
-			contentList = o.firstChild.windowClassName
-			if contentList == 'SysHeader32':
-				ui.message(_("The list of remote files cannot be found."))
-			else:
-				o.setFocus()
-	except No version information:
-		pass
+			o.setFocus()
+	else:
+		o = getChildByID(fg, ID = -31828, nb=1)
+		o = getChildByID(o, ID = -31828, nb=2)
+		o = getChildByID(o, ID = -31827, nb=3)
+		o = getChildByID(o, ID = -31827, nb=4)
+		o = getChildByID(o, ID = -31818, nb=5)
+		o = getChildByID(o, ID = -31818, nb=6)
+		o = getChildByID(o, ID = -31817, nb=7)
+		o = getChildByID(o, ID = -31817, nb=8)
+		o = getChildByID(o, ID = -31815, nb=9)
+		o = getChildByID(o, ID = -31815, nb=10)
+		o = getChildByID(o, ID = -31808, nb=11)
+		o = getChildByID(o, ID = -31808, nb=12)
+		o = getChildByID(o, ID = -31806, nb=13)
+		o = getChildByID(o, ID = -31806, nb=14)
+		contentList = o.firstChild.windowClassName
+		if contentList == 'SysHeader32':
+			ui.message(_("The list of remote files cannot be found."))
+	#Sinon on y place le focus
+		else:
+			o.setFocus()
